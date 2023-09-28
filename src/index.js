a8e53ba40fc2c2c62d028d0ea058864806d8e67b
Victor John Makanda <victormakanda254@gmail.com>
2023-09-28 20:57:23 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      handleForm(e.target.description1.value);
+      form.reset();
+    });
+  
 });
+
+function handleForm(todo){
+  let li= document.createElement('li');
+  let btn= document.createElement('button')
+  btn.addEventListener('click', function handleDelete(e){
+    e.target.parentNode.remove();
+  })
+
+  li.textContent=`${todo} `;
+  btn.textContent= 'delete'
+  btn.style.fontSize="10px";
+
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+
+
+}
+
